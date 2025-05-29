561745b1461357b47d3d6bf19b58b3a76e19fee8
BuildTools <anikpatel1322@gmail.com>
2025-05-29 15:04:18 -0400

    tt fix maybe bench 3035577

--- src/tt.h
+++ src/tt.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include "external/chess.hpp"
+#include "parameters.h"
 #include <vector>
+#include <thread>
+#include <bitset>
+#include <climits>
+#include <cstring>
+#include <iostream>
 
 using namespace chess;
 
@@ -35,30 +41,47 @@ struct TTEntry {
 
 struct TTable {
 private:
-	std::vector<TTEntry> table;
+	TTEntry *table;
 public:
 	uint64_t size;
 
 	TTable(uint64_t sizeMB = 16){
-
+		table = nullptr;
 		resize(sizeMB);
 	}
-	void clear(){
-		std::fill(table.begin(), table.end(), TTEntry{});
+	void clear(size_t threadCount = 1){
+		std::vector<std::thread> threads;
+
+		auto clearTT = [&](size_t threadId) {
+			// The segment length is the number of entries each thread must clear
+			// To find where your thread should start (in entries), you can do threadId * segmentLength
+			// Converting segment length into the number of entries to clear can be done via length * bytes per entry
+
+			size_t start = (size * threadId) / threadCount;
+			size_t end   = std::min((size * (threadId + 1)) / threadCount, size);
+
+			std::memset(table + start, 0, (end - start) * sizeof(TTEntry));
+		};
+
+		for (size_t thread = 1; thread < threadCount; thread++)
+			threads.emplace_back(clearTT, thread);
+
+		clearTT(0);
+
+		for (std::thread& t : threads)
+			if (t.joinable())
+				t.join();
 	}
 	void resize(uint64_t MB){
 		size = MB * 1024 * 1024 / sizeof(TTEntry);
-		if (size == 0)
-			size++;
-		table.resize(size);
+		if (table != nullptr)
+			std::free(table);
+		table = static_cast<TTEntry*>(std::malloc(size * sizeof(TTEntry)));
 	}
 	uint64_t index(uint64_t key) { 
-		return key % size;
+		return static_cast<std::uint64_t>((static_cast<u128>(key) * static_cast<u128>(size)) >> 64);
 	}
 
-	void setEntry(TTEntry entry){
-		table[index(entry.zobrist)] = entry;
-	}
 	TTEntry *getEntry(uint64_t key){
 		return &table[index(key)];
 	}
