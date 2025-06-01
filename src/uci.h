9877c7701d230ac0efdd69986ebbe81b333fcf2c
BuildTools <anikpatel1322@gmail.com>
2025-06-01 18:42:04 -0400

    first spsa tune 3k iters bench 2426516

--- src/uci.h
+++ src/uci.h
@@ -2,12 +2,11 @@
 
 #include "search.h"
 #include <string.h>
+#include <iostream>
 
 // Yoinked from Weiss
 // https://github.com/TerjeKir/weiss/blob/v1.0/src/uci.h
-
 #define INPUT_SIZE 8192
-
 enum InputCommands {
     // UCI
     GO          = 11,
@@ -24,32 +23,25 @@ enum InputCommands {
     PRINT       = 112,
     DATAGEN     = 124
 };
-
 bool GetInput(char *str) {
     memset(str, 0, INPUT_SIZE);
     if (fgets(str, INPUT_SIZE, stdin) == NULL)
         return false;
-
     str[strcspn(str, "\r\n")] = '\0';
-
     return true;
 }
-
 bool BeginsWith(const char *str, const char *token) {
     return strstr(str, token) == str;
 }
-
 // Tests whether the name in the setoption string matches
 bool OptionName(const char *str, const char *name) {
     return BeginsWith(strstr(str, "name") + 5, name);
 }
-
 // Returns the (string) value of a setoption string
 char *OptionValue(const char *str) {
     const char *valuePtr = strstr(str, "value");
     return (valuePtr != nullptr && strlen(valuePtr) > 6) ? (char *)(valuePtr + 6) : nullptr;
 }
-
 // Sets a limit to the corresponding value in line, if any
 void SetLimit(const char *str, const char *token, int64_t *limit) {
     const char *ptr = strstr(str, token);
@@ -59,3 +51,55 @@ void SetLimit(const char *str, const char *token, int64_t *limit) {
 }
 
 void bench(Searcher *searcher);
+
+
+// ------------------------- ASCI ART -------------------------------
+
+void tarnishedAscii() {
+    std::cout << R"(
+                         :=*#*+:                      
+                     +#***********#                   
+                  +#****************#                 
+               .#*#******************@.               
+           .:=#*%#********************%%:             
+               :*************%***#****@*%             
+              :%**************%**@*#*%**%             
+        +:   .%*********#*****#*#@*#****%             
+       ##   :@************#@*#*#%##**%*@:             
+      :**#=##*****#**#=.-:%*@%#%#*##**%               
+      :*******%*%***%-   #***@@%##*#@.                
+       ##****%#**%*%.    =%======%+                   
+        .=%%%%*-#=.     .%+=----=+%=                  
+                     -@#--*+--::=*--#%-               
+                   =#=--=*-#=-:-#-*---=%=             
+                  #*-----++-%--%=*=-----**            
+                  %:------#=%--%=*-------%            
+                  %-------=+#=-**=------=#            
+                 :%@%-=*--=+*==+*=--#=-#@@:           
+                .%@=-=**+*%%#++*%%#+*+--=@@           
+                 %%#+=----------------=+#%@           
+                 +*%+%%@@%*======*%@@%%=%+#           
+                 :%@==%#*%%%%%%%%%@##%==@#-           
+                  =@+*%#==+++==+++==#%+=@#            
+                   +=****+*#+--+#*+****=+             
+                   =**@*%=%*%-:%*%=%*%**=             
+                 .%@#*@*%=@+%-:%*@=%*@+%@#            
+               .%+-+@=@*%*@+%-:%*@+%*@=@+:*%.         
+            =%*--==*#@#=@*##%--%*#*@=#%%+=---#%=      
+            #=#==+#+==+%#==%%=-%#+=%%+==+#+==*-#      
+            +*+%#=------=*%%@==@%%*=------+*%+*=      
+             =#=#+----------=*+=----------+#=#-       
+               #+=%+---------+=---------*%=+#         
+                 **=*%------+#++-----=%+=#*           
+                   :#*=*%=:::+=---+%*=##:             
+                      :*#+*#+***#*+#*:                
+                         :##=**=##:                   
+                            =%%-                      
+ _______       _____  _   _ _____  _____ _    _ ______ _____  
+|__   __|/\   |  __ \| \ | |_   _|/ ____| |  | |  ____|  __ \ 
+   | |  /  \  | |__) |  \| | | | | (___ | |__| | |__  | |  | |
+   | | / /\ \ |  _  /| . ` | | |  \___ \|  __  |  __| | |  | |
+   | |/ ____ \| | \ \| |\  |_| |_ ____) | |  | | |____| |__| |
+   |_/_/    \_\_|  \_\_| \_|_____|_____/|_|  |_|______|_____/                                    
+    )" << "\n" << std::endl;
+}
