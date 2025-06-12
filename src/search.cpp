5641727acd3f55992bd5009d64ca7e1d3d081aad
BuildTools <anikpatel1322@gmail.com>
2025-06-12 19:04:05 -0400

    quiet history pruning bench 2396092

--- src/search.cpp
+++ src/search.cpp
@@ -509,14 +509,11 @@ namespace Search {
 				if (!SEE(thread.board, move, SEE_PRUNING_SCALAR() * depth))
 					continue;
 
-				// History Pruning
-				// Failed, will test again later
-				// https://github.com/aronpetko/integral/blob/733036df88408d0d6338d05f7991f46f0527ed4f/src/engine/search/search.cc#L945
-				// https://github.com/mcthouacbb/Sirius/blob/15501c19650f53f0a10973695a6d284bc243bf7d/Sirius/src/search.cpp#L614
-				// if (isQuiet && depth <= HIST_PRUNING_DEPTH && ss->historyScore <= HIST_PRUNING_MARGIN * depth){
-				// 	skipQuiets = true;
-				// 	continue;
-				// }
+				// Quiet History Pruning
+				if (isQuiet && depth <= HIST_PRUNING_MAX_DEPTH() && ss->historyScore < HIST_PRUNING_QUIET_MARGIN() + HIST_PRUNING_QUIET_SCALE() * depth){
+					skipQuiets = true;
+					continue;
+				}
 			}
 
 			// Singular Extensions
