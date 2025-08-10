37c791450c47a079d1667649110a3f2c008c30a7
BuildTools <anikpatel1322@gmail.com>
2025-08-10 19:18:59 -0400

    bench 1917462

--- src/search.cpp
+++ src/search.cpp
@@ -366,7 +366,9 @@ namespace Search {
         // Pruning
         if (!root && !isPV && !inCheck && moveIsNull(ss->excluded)) {
             // Reverse Futility Pruning
-            int rfpMargin = RFP_SCALE() * (depth - improving);
+            int rfpMargin = RFP_SCALE() * depth 
+                            - RFP_SCALE() * improving
+                            - 8 * (cutnode && !ttHit);
             rfpMargin += corrplexity * RFP_CORRPLEXITY_SCALE() / 128;
 
             if (depth <= 8 && ss->eval - rfpMargin >= beta)
