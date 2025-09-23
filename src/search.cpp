c08b4521b514538889210154035c03fd9509c2e6
BuildTools <anikpatel1322@gmail.com>
2025-09-23 11:53:23 -0400

    bench 2799655

--- src/search.cpp
+++ src/search.cpp
@@ -505,7 +505,7 @@ namespace Search {
                 // Reckless idea 
                 // Bad noisy futility pruning
                 futility = ss->staticEval + BNFP_DEPTH_SCALE() * depth + BNFP_MOVECOUNT_SCALE() * moveCount / 128;
-                if (!inCheck && depth <= 5 && picker.stage == MPStage::BAD_NOISY && std::abs(alpha) < 2000 && futility <= alpha) {
+                if (!inCheck && lmrDepth <= 5 && picker.stage == MPStage::BAD_NOISY && std::abs(alpha) < 2000 && futility <= alpha) {
                     if (!isMateScore(bestScore) && bestScore <= futility)
                         bestScore = futility;
                     break;
