07ab097eb606ad27b94a15ad0297b92f8ef91259
BuildTools <anikpatel1322@gmail.com>
2025-11-01 17:04:43 -0400

    bench 2992576

--- src/search.cpp
+++ src/search.cpp
@@ -407,7 +407,7 @@ namespace Search {
             rfpMargin -= RFP_IMPROVING_SCALE() * improving;
             rfpMargin += corrplexity * RFP_CORRPLEXITY_SCALE() / 128;
 
-            if (depth <= 8 && ss->eval - rfpMargin >= beta)
+            if (depth <= 8 && ttData.bound != TTFlag::FAIL_LOW && ss->eval - rfpMargin >= beta)
                 return (ss->eval + beta) / 2;
 
             if (depth <= 4 && std::abs(alpha) < 2000 && ss->staticEval + RAZORING_SCALE() * depth <= alpha) {
